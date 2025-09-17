e8b4eb30241d47ca57a7099d201bd333891b5a59
Suhyeon Jeong <112jshjsh@kookmin.ac.kr>
2025-09-17 16:25:38 +0900

    feat: style dictionary

---
+++ sd-tokens-config.js
@@ -0,0 +1,25 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
+  source: ['./tokens/flatten/global-token.json'], 
+  preprocessors: ['tokens-studio'],
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath: 'tokens',
+      files: [
+        {
+          destination: 'tokens.json',
+          format: 'json',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
